ad1df52615b00dd25fe01d4bcc337f3319ec1236
pkouliev <pkouliev@gmail.com>
2020-12-14 21:56:36 -0500

    day1 and day2

---
+++ src/test/java/tests/day2/BasicNavigation.java
@@ -0,0 +1,43 @@
+package tests.day2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicNavigation {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // if you have exception: cannot load a class
+        // that means that package name doesn't match with
+        // location of the class itself
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+
+        // to maximize browser
+        driver.manage().window().maximize();
+
+        driver.get("http:google.com");
+        Thread.sleep(3000);
+
+        // we want to navigate to the different page
+        // within same browser
+        // we are not gonna open new browser or new tab
+        // URL can be passed as an object
+        // or as a string
+        // we use string
+        driver.navigate().to("http://amazon.com");
+        Thread.sleep(3000);
+
+        // if I want to comeback to the the previous page
+        driver.navigate().back();
+        Thread.sleep(3000);
+
+        // if I want to know URL of current website
+        String url = driver.getCurrentUrl();
+        System.out.println(url);
+
+        // selenium cannot close browser automatically
+        // for this, we use method close()
+        driver.close();
+    }
+}
