ab05d9c7812a993fd66a74e287f3dd86955008a5
pkouliev <pkouliev@gmail.com>
2020-12-15 13:46:20 -0500

    BrowserFactoryTest class created to test BrowserFactory utillity class by calling getDriver() method and providing parameter reference with browser name.

---
+++ src/test/java/tests/day3/BrowserFactoryTest.java
@@ -0,0 +1,27 @@
+package tests.day3;
+
+import org.openqa.selenium.WebDriver;
+
+import static utils.BrowserFactory.*;
+
+public class BrowserFactoryTest {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // getting WebDriver object (driver) using getDriver() method from BrowserFactory class and
+        // providing reference variable/parameter (browser name)
+        WebDriver driver = getDriver("chrome");
+
+        driver.get("http://practice.cybertekschool.com");
+
+        // print a source code of the page
+        System.out.println(driver.getPageSource());
+        // to finish test execution
+
+        Thread.sleep(5000);
+
+        driver.quit();
+
+
+    }
+}
