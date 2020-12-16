3401cf082e073da7e3a11bafa516b78fc2cf4c80
pkouliev <pkouliev@gmail.com>
2020-12-16 14:21:26 -0500

    Added getDriver() method to BrowserUtils class as well.

--- src/test/java/utils/BrowserUtils.java
+++ src/test/java/utils/BrowserUtils.java
@@ -1,5 +1,10 @@
 package utils;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class BrowserUtils {
 
     // This method will be used to pause our test execution
@@ -11,5 +16,16 @@ public class BrowserUtils {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    public static WebDriver getDriver(String browser) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+        }
+        return null;
     }
 }
