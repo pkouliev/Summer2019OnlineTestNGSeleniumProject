b3cec1101cea4e106d1320baf7802b1ba4f06039
pkouliev <pkouliev@gmail.com>
2020-12-16 08:24:48 -0500

    FindElementsTest class is created introducing WebElement interface and setting up element object using WebDriver interface method findElement() and introducing By interface with its own element reading methods and utilizing one of locators, id.

---
+++ src/test/java/tests/day4/FindElementsTest.java
@@ -0,0 +1,30 @@
+package tests.day4;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import utils.BrowserFactory;
+import utils.BrowserUtils;
+
+public class FindElementsTest {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = BrowserFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+
+        driver.get("http://practice.cybertekschool.com/forgot_password");
+        BrowserUtils.wait(2);
+
+        // Step 1. Open inspector in chrome and find the locator for the element
+        // Step 2. Create object of webElement
+        // Step 3. Use webelement
+        WebElement button = driver.findElement(By.id("form_submit"));
+        // to click on the element
+        button.click();
+
+        BrowserUtils.wait(3);
+
+        driver.quit();
+    }
+}
