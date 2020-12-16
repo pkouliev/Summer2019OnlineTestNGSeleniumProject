46b7250869a9db707842fd2a22272eee774d0415
pkouliev <pkouliev@gmail.com>
2020-12-16 09:56:24 -0500

    In this task, we used another locator name to access WebElement we called inputBox, then used sendKeys() method to imput text and submit using another element we called button and execute click() method.

---
+++ src/test/java/tests/day4/EnterTextPractice.java
@@ -0,0 +1,29 @@
+package tests.day4;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import utils.BrowserFactory;
+import utils.BrowserUtils;
+
+public class EnterTextPractice {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = BrowserFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("http://practice.cybertekschool.com/forgot_password");
+        BrowserUtils.wait(2);
+
+        WebElement inputBox = driver.findElement(By.name("email"));
+
+        // we enter text
+        inputBox.sendKeys("random@email.com");
+
+        WebElement button = driver.findElement(By.id("form_submit"));
+        button.click();
+        BrowserUtils.wait(2);
+
+        driver.quit();
+    }
+}
