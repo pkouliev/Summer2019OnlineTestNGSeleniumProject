07a1c2d6fa2936ef72ed6a6af059aa1b0ce490ab
pkouliev <pkouliev@gmail.com>
2020-12-16 14:55:44 -0500

    In this test, we used getAttribute() to get a value of the specific attribute from specific webelement. Also, we used same method to read value attribute from input box. As alternative to click() method we utilized submit() method based on type with value "submit" is provided.

---
+++ src/test/java/tests/day4/ReadAttributesTest.java
@@ -0,0 +1,39 @@
+package tests.day4;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import utils.BrowserUtils;
+
+public class ReadAttributesTest {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = BrowserUtils.getDriver("chrome");
+        driver.manage().window().maximize();
+        BrowserUtils.wait(2);
+
+        driver.get("http://practice.cybertekschool.com/forgot_password");
+        BrowserUtils.wait(2);
+
+        WebElement inputBox = driver.findElement(By.name("email"));
+
+        // to read value of any attribute
+        // name = "email" name is an attribute, email is a value of the attribute
+        System.out.println(inputBox.getAttribute("name"));
+
+        inputBox.sendKeys("random_email@email.com");
+        // how to read entered text?
+        // it's gonna be inside value attribute
+        System.out.println(inputBox.getAttribute("value"));
+
+        // if button has a type "submit"
+        // we can use .submit() method instead of click() as well
+        WebElement passwordButton = driver.findElement(By.id("form_submit"));
+        passwordButton.submit();
+        // alternative to click(),
+        // only if there is attribute type with value "submit", type="submit"
+
+        driver.quit();
+    }
+}
