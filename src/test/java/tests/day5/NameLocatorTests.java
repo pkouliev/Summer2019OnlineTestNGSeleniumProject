c0233498ec8b2e45567a343c00d753295eddde55
pkouliev <pkouliev@gmail.com>
2020-12-17 10:08:43 -0500

    Name locator is used in this class tests. Also, other methods like sendKeys(), click(), submit() may get combined in same line for execution unless we want to save value of element into variable to be reused later which could be beneficial later for testing framework.

---
+++ src/test/java/tests/day5/NameLocatorTests.java
@@ -0,0 +1,34 @@
+package tests.day5;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import utils.BrowserUtils;
+
+public class NameLocatorTests {
+
+    public static void main(String[] args) {
+
+        // remember!
+        // for Mac users: there is no need to use WebDriverManager for Safari
+        // Safari has embedded webdriver support.
+        WebDriver driver = BrowserUtils.getDriver("chrome");
+        driver.manage().window().maximize();
+        BrowserUtils.wait(2);
+
+        driver.get("http://practice.cybertekschool.com/sign_up");
+        BrowserUtils.wait(2);
+
+        // if you want to do in one line without creating webelement reference variable
+        // enter full name
+        driver.findElement(By.name("full_name")).sendKeys("Test User");
+        // enter email
+        driver.findElement(By.name("email")).sendKeys("test_email@email.com");
+        // click sign up
+        driver.findElement(By.name("wooden_spoon")).submit();
+
+        BrowserUtils.wait(2);
+        driver.quit();
+
+
+    }
+}
