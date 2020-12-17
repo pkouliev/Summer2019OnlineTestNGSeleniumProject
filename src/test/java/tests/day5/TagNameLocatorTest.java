77bf7c6c56cc614e84c4caacafb96247c448bb6f
pkouliev <pkouliev@gmail.com>
2020-12-17 12:48:38 -0500

    Used tagName locator to locate the element and retrieve the text.

---
+++ src/test/java/tests/day5/TagNameLocatorTest.java
@@ -0,0 +1,35 @@
+package tests.day5;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import utils.BrowserUtils;
+
+public class TagNameLocatorTest {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = BrowserUtils.getDriver("chrome");
+        assert driver != null;
+        driver.manage().window().maximize();
+        BrowserUtils.wait(2);
+
+        driver.get("http://practice.cybertekschool.com/sign_up");
+        BrowserUtils.wait(2);
+
+        // if you want to do in one line without creating webelement reference variable
+        // enter full name
+        driver.findElement(By.name("full_name")).sendKeys("Test User");
+        // enter email
+        driver.findElement(By.name("email")).sendKeys("test_email@email.com");
+        // click sign up
+        driver.findElement(By.name("wooden_spoon")).submit();
+        BrowserUtils.wait(2);
+
+        WebElement message = driver.findElement(By.tagName("h3"));
+        System.out.println(message.getText());
+
+        BrowserUtils.wait(2);
+        driver.quit();
+    }
+}
