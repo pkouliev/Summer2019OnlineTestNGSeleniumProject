6e22ee48ffa66096bb09b182f3c18e93047415a5
pkouliev <pkouliev@gmail.com>
2020-12-17 11:11:51 -0500

    Finding element by className locator and printing the text of the element.

---
+++ src/test/java/tests/day5/ClassNameLocatorTests.java
@@ -0,0 +1,31 @@
+package tests.day5;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import utils.BrowserUtils;
+
+public class ClassNameLocatorTests {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = BrowserUtils.getDriver("chrome");
+        driver.manage().window().maximize();
+        BrowserUtils.wait(2);
+
+        driver.get("http://practice.cybertekschool.com/multiple_buttons");
+        BrowserUtils.wait(2);
+
+        // <h3 class="h3">Multiple buttons</h3>
+        // h3 - it's a class name, or value of class attribute
+        // if class has text with a space, that means there are more
+        WebElement heading = driver.findElement(By.className("h3"));
+
+        // we can read the text of the element
+        System.out.println(heading.getText());
+
+
+        BrowserUtils.wait(2);
+        driver.quit();
+    }
+}
