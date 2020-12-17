d9ae3f74fab8b48cc3baefe4bc5692eaa08a4007
pkouliev <pkouliev@gmail.com>
2020-12-17 00:08:12 -0500

    In this task, we use ID locator to find element for button, then still use id as locator to get result and print text.

---
+++ src/test/java/tests/day5/IDLocatorTests.java
@@ -0,0 +1,31 @@
+package tests.day5;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import utils.BrowserUtils;
+
+public class IDLocatorTests {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = BrowserUtils.getDriver("chrome");
+        driver.manage().window().maximize();
+        BrowserUtils.wait(2);
+
+        driver.get("http://practice.cybertekschool.com/multiple_buttons");
+        BrowserUtils.wait(2);
+
+        WebElement specialButton = driver.findElement(By.id("disappearing_button"));
+        specialButton.click();
+        BrowserUtils.wait(2);
+
+        WebElement result = driver.findElement(By.id("result"));
+        System.out.println(result.getText());
+        BrowserUtils.wait(2);
+
+        driver.quit();
+
+
+    }
+}
