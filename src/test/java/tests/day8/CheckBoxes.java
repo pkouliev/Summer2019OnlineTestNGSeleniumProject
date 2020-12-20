0c06953c36a0c0375f965f1d19140390476f7541
pkouliev <pkouliev@gmail.com>
2020-12-19 22:36:47 -0500

    Another test practice this time using page with checkboxes and still using same approach as with page with radio buttons, finding element or making list of elements and then carrying test actions using if else statements with isEnabled() and isSelected() methods or conditions.

---
+++ src/test/java/tests/day8/CheckBoxes.java
@@ -0,0 +1,63 @@
+package tests.day8;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import utils.BrowserUtils;
+
+import java.util.List;
+
+public class CheckBoxes {
+
+    // command + option + L - to organize code for Mac
+    // control + alt + L - to organize code for windows
+
+    private WebDriver driver;
+    // private because it will be used in this class
+
+    @BeforeMethod
+    public void setup() {
+        driver = BrowserUtils.getDriver("chrome");
+        assert driver != null;
+        BrowserUtils.wait(1);
+        driver.get("http://practice.cybertekschool.com/");
+        BrowserUtils.wait(1);
+
+        // <a href="/checkboxes">Checkboxes</a>
+        driver.findElement(By.linkText("Checkboxes")).click();
+        BrowserUtils.wait(1);
+    }
+
+    @Test
+    public void test1() {
+        // find all checkboxes
+        // any chackbox will have type="checkbox"
+        List<WebElement> checkboxes = driver.findElements(By.cssSelector("[type='checkbox']"));
+        int index = 1;
+        for (WebElement checkbox : checkboxes) {
+            if (checkbox.isEnabled() && !checkbox.isSelected()) {
+                checkbox.click();
+                BrowserUtils.wait(1);
+                System.out.println("Checkbox " + index + " is clicked \n");
+            } else {
+                System.out.println("Checkbox " + index + " is not clicked \n");
+            }
+            index++;
+        }
+
+
+    }
+
+    @AfterMethod
+    public void teardown() {
+        BrowserUtils.wait(1);
+        driver.quit();
+    }
+}
+
+
+
+
