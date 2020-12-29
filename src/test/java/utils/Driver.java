1ccbfaf0f76613e02cb9d51940a7742998942d52
pkouliev <pkouliev@gmail.com>
2020-12-29 00:08:06 -0500

    Singleton WebDriver object driver created to be passed within static getDriver() method and used by all tests instead of setting up new browser driver everytime. Switch statement was used to alternate between arguments provided to "browser" key in configuration properties. Also custom close() method added to quit driver and destroy it as part of @AfterMethod later created within TestBase class along with @BeforeMethod referencing to this class to create driver object.

---
+++ src/test/java/utils/Driver.java
@@ -0,0 +1,51 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Driver {
+    private static WebDriver driver;
+
+    // you cannot do like this, if constructor is private Driver obj = new Driver()
+    private Driver() {
+
+    }
+
+    // if switch statement complains on String parameter
+    // change java version to the latest.
+    // File -> Project Structure -> Set Project language level to the latest
+    public static WebDriver getDriver() {
+        // if WebDriver object was not created yet
+        if (driver == null) {
+            // create WebDriver object based on browser value from properties
+            String browser = ConfigurationReader.getValue("browser");
+            // if browser type is wrong, stop tests and throw exception
+            // no browser will be opened
+            switch (browser) {
+                case "chrome" -> {
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                }
+                case "firefox" -> {
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                }
+                default -> throw new RuntimeException("Wrong browser type");
+            }
+        }
+        // if WebDriver object was created - you can use it
+        return driver;
+    }
+
+    public static void close() {
+        // if driver still exists
+        if (driver != null) {
+            // close all browsers
+            driver.quit();
+            // destroy driver object, ready for garbage collector (gc)
+            driver = null;
+        }
+    }
+}
