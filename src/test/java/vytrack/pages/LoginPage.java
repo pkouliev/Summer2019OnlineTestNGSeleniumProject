4ab34d815fb662b6fa80e0ee248d456d40e48790
pkouliev <pkouliev@gmail.com>
2020-12-30 18:49:52 -0500

    PageFactory class used to support FindBy() class methods in declaring and initializing web elements. Also login method which is same all tests in this app, added log in into app before proceeding to actual tests.

---
+++ src/test/java/vytrack/pages/LoginPage.java
@@ -0,0 +1,48 @@
+package vytrack.pages;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.Driver;
+
+// according to page object model design
+// we have to create corresponding page class
+// for each page of application
+// Login page = LoginPage class
+// every page class will store web elements and methods related to that page
+public class LoginPage extends BasePage {
+
+    @FindBy(id = "prependedInput") // this line will initialize web element
+    public WebElement userNameInput;
+
+    @FindBy(id = "prependedInput2") // without @FindBy, web element will be null
+    public WebElement passwordInput;
+
+    @FindBy(id = "_submit")
+    public WebElement loginButton;
+
+    @FindBy(className = "alert alert-error")
+    public WebElement warningMessage;
+
+    public LoginPage() {
+        // it's mandatory if you want to use @FindBy annotation
+        // this means LoginPage class
+        // Driver.getDriver() return WebDriver object
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    /**
+     * reusable login method
+     * just call this method to login, provide userName and password as parameters
+     *
+     * @param userName userName is provided within test once login method called.
+     * @param password password is provided within test once login method called.
+     */
+    public void login(String userName, String password) {
+        userNameInput.sendKeys(userName);
+        // Keys.ENTER to replace login button click
+        passwordInput.sendKeys(password, Keys.ENTER);
+    }
+
+}
