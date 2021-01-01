369a85abd46c254717ed8a7c230d54e5fe7d717e
pkouliev <pkouliev@gmail.com>
2021-01-01 10:30:20 -0500

    In this test, we verify owners name retrieved from web element matched expected one.

---
+++ src/test/java/vytrack/tests/CreateCalendarEventTests.java
@@ -0,0 +1,26 @@
+package vytrack.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import vytrack.pages.CreateCalendarEventPage;
+
+public class CreateCalendarEventTests extends CreateCalendarEventPage {
+
+    @Test(description = "Verify owners name equals to Stephan Tremaine Haley (it works on qa1 storemanager85 ")
+    public void test1() {
+
+        // login as Stephan Tremaine Haley (storemanager85)
+        login("storemanager85", "UserUser123");
+
+        //go to calendar events page
+        navigateTo("Activities", "Calendar Events");
+
+        // click on create calendar event button
+        clickToCreateCalendarEvent();
+
+        String expectedOwner = "Stephan Tremaine Haley";
+        String actualOwner = getOwnerName(expectedOwner).trim();
+
+        Assert.assertEquals(actualOwner, expectedOwner);
+    }
+}
