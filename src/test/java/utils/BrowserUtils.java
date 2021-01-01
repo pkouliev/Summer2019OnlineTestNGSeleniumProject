66dfcd4c93cbc8776f8ba098ee4e3c51c6fdb778
pkouliev <pkouliev@gmail.com>
2021-01-01 10:11:36 -0500

    Updated with 4 reusable utility methods to support page and test classes and methods.

--- src/test/java/utils/BrowserUtils.java
+++ src/test/java/utils/BrowserUtils.java
@@ -1,11 +1,17 @@
 package utils;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class BrowserUtils {
+public class BrowserUtils extends Driver {
+    static WebDriverWait wait;
 
     // This method will be used to pause our test execution
     // provide number of seconds as a parameters
@@ -33,4 +39,62 @@ public class BrowserUtils {
 
         System.out.println("\n================================================================================================\n");
     }
+
+    /**
+     * @param element web element
+     */
+    public static void waitForStaleElement(WebElement element) {
+        int y = 0;
+        while (y <= 15) {
+            try {
+                element.isDisplayed();
+                break;
+            } catch (StaleElementReferenceException st) {
+                System.out.println(st.getMessage());
+
+            }
+
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            y++;
+        }
+    }
+
+    /**
+     * Waits for the provided element to be visible on the page
+     *
+     * @param element         page elements
+     * @param timeToWaitInSec time in seconds
+     * @return element once visible
+     */
+    public static WebElement waitForVisibility(WebElement element, int timeToWaitInSec) {
+        WebDriverWait wait = new WebDriverWait(getDriver(), timeToWaitInSec);
+        return wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    /**
+     * Clicks on an element using JavaScript
+     *
+     * @param element page element needs to be clicked
+     */
+    public static void clickWithJS(WebElement element) {
+        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].scrollIntoView(true);", element);
+        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].click();", element);
+    }
+
+    /**
+     * Waits for provided element to be clickable
+     *
+     * @param element page element
+     * @param timeout time to wait
+     * @return element once clickable
+     */
+    public static WebElement waitForClickability(WebElement element, int timeout) {
+        wait = new WebDriverWait(getDriver(), timeout);
+        return wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
 }
