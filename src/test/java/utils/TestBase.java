ed8562c789bcd643624544125018cb70210f4507
pkouliev <pkouliev@gmail.com>
2021-01-02 22:20:59 -0500

    ExtentReports...Reporter...Test classes are setup into @BeforeTest method to prepare report config before tests. And flush() method used @AfterTest to make reports available. ITestResult interface methods used in @AfterMethod to setup conditions to get test results or skip them if test fails.

--- src/test/java/utils/TestBase.java
+++ src/test/java/utils/TestBase.java
@@ -1,19 +1,54 @@
 package utils;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
-import java.util.concurrent.TimeUnit;
+import java.io.IOException;
 
 // this class will be a test foundation for all test classes
 // we will put here only before and after parts
 // In this way before and after methods will be the same
 // Every test class will extend TestBase class
-public class TestBase extends Driver {
-    public WebDriver driver = getDriver();
-    public WebDriverWait wait = new WebDriverWait(driver, 10);
+public class TestBase extends BrowserUtils {
+
+
+    /*
+    ExtentReports itself does not build any reports, but allows reporters to access information,
+    which in turn build the said reports. An example of building an HTML report and adding information to ExtentX:
+    ExtentHtmlReporter html = new ExtentHtmlReporter("Extent.html");
+    ExtentXReporter extentx = new ExtentXReporter("localhost");
+     */
+    protected ExtentReports extentReports;
+    // The ExtentSparkReporter creates a rich standalone spark file.
+    protected ExtentSparkReporter extentSparkReporter;
+    // Defines a test. You can add logs, snapshots, assign author and categories to a test and its children.
+    protected ExtentTest extentTest;
+
+    @BeforeTest
+    public void beforeTest() {
+        // location of report
+        // it's gonna be next to target folder, test-output folder
+        String filePath = System.getProperty("user.dir") + "/test-output/report.html";
+        extentReports = new ExtentReports();
+        extentSparkReporter = new ExtentSparkReporter(filePath);
+        extentReports.attachReporter(extentSparkReporter);
+        extentSparkReporter.config().setReportName("Vytrack Test Results");
+        extentReports.setSystemInfo("Environment", "QA1");
+        extentReports.setSystemInfo("Browser", ConfigurationReader.getValue("browser"));
+        extentReports.setSystemInfo("OS", System.getProperty("os.name"));
+    }
+
+    @AfterTest
+    public void afterTest() {
+        // Writes test information from the started reporters to their output view
+        extentReports.flush();
+    }
 
 
     @BeforeMethod
@@ -25,7 +60,25 @@ public class TestBase extends Driver {
     }
 
     @AfterMethod
-    public void teardown() {
+    // ITestResult class describes the result of a test. (in TestNG)
+    public void teardown(ITestResult result) {
+
+        if (result.getStatus() == ITestResult.FAILURE) {
+            extentTest.fail(result.getName());
+            extentTest.fail(result.getThrowable());
+            try {
+                // getScreenshot(result.getName() - takes screenshot and returns location of that screenshot
+                // this method throws IOException (which is checked exception)
+                // any checked eception must be handled immediately
+                extentTest.addScreenCaptureFromPath(getScreenshot(result.getName()));
+            } catch (IOException e) {
+                // print error info
+                e.printStackTrace();
+            }
+        } else if (result.getStatus() == ITestResult.SKIP) {
+            extentTest.skip("Test case was skipped: " + result.getName());
+        }
+
         Driver.close();
     }
 }
