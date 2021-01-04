f37eecfb3166ee13ccd6cc19c3b1cf9f8a1c7ced
pkouliev <pkouliev@gmail.com>
2021-01-04 15:57:14 -0500

    Updated

--- src/test/java/vytrack/tests/LoginTests.java
+++ src/test/java/vytrack/tests/LoginTests.java
@@ -1,34 +1,32 @@
 package vytrack.tests;
 
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import utils.ConfigurationReader;
+import utils.Driver;
+import utils.TestBase;
 import vytrack.pages.LoginPage;
 
-// we write extends TestBase to inherit @BeforeMethod and @AfterMethod
-// this class will be dedicated to tests related to Login page only
-// we don't have to find elements here
-// we should find elements in page classes only
-public class LoginTests extends LoginPage {
+//we write extends tests.TestBase to inherits @before and @after methods
+//this class will be dedicated to tests related to login page only
+//we don't have to find elements here
+//we should find elements in page classes only
+public class LoginTests extends TestBase {
 
-    @Test(description = "Verify that page title is 'Dashboard'")
+    @Test(description = "Verify that page title is a 'Dashboard'")
     public void test1() {
-        // create page object
-        //LoginPage loginPage = new LoginPage();
-
-        // provide userName and password
-        //login("storemanager85", "UserUser123");
-        loginQA1();
-
-        // this is an explicit wait
-        // it waits until title is 'Dashboard'
+        //create page object
+        LoginPage loginpage = new LoginPage();
+        //call login method
+        //provide username and password
+        loginpage.login(ConfigurationReader.getValue("user_name"), ConfigurationReader.getValue("password"));
+        /*
+        WebDriverWait wait = new WebDriverWait(Driver.get(),10);
+        //this is an explicit wait
+        //it waits until title is ''Dashboard
         wait.until(ExpectedConditions.titleIs("Dashboard"));
-        // verification stage
-        // Driver.get() = driver same things
-        // Driver.get() = return WebDriver object
-        String title = driver.getTitle();
-        Assert.assertEquals(title, "Dashboard", "Test failed. Title doesn't match");
-        System.out.println("Title: " + title + " matches. Test passed");
-
+        */
+        //verification page
+        Assert.assertEquals(Driver.getDriver().getTitle(), "Dashboard");
     }
 }
