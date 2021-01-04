a4c520248c32664e390c468148cb205b8a3782cd
pkouliev <pkouliev@gmail.com>
2021-01-04 15:43:25 -0500

    textToBePresentInElement() method removed

--- src/test/java/vytrack/pages/CalendarEventsPage.java
+++ src/test/java/vytrack/pages/CalendarEventsPage.java
@@ -2,19 +2,16 @@ package vytrack.pages;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import utils.BrowserUtils;
 
-public class CalendarEventsPage extends LoginPage {
+public class CalendarEventsPage extends BasePage {
 
     @FindBy(css = "[title='Create Calendar event']")
     public WebElement createCalendarEvent;
 
     public void clickToCreateCalendarEvent() {
-        //BrowserUtils.waitForVisibility(createCalendarEvent, 5);
-        BrowserUtils.waitForClickability(createCalendarEvent, 5);
-        wait.until(ExpectedConditions.textToBePresentInElement(createCalendarEvent, createCalendarEvent.getText()));
+        BrowserUtils.waitForVisibility(createCalendarEvent, 5);
+        BrowserUtils.waitForClickablility(createCalendarEvent, 5);
         createCalendarEvent.click();
-        waitUntilLoaderMaskDisappear();
     }
 }
