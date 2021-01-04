e5be5c5846ce3227a1509417c9ca4d8724798a2e
pkouliev <pkouliev@gmail.com>
2021-01-04 15:55:01 -0500

    Updated

--- src/test/java/vytrack/tests/CreateCalendarEventTests.java
+++ src/test/java/vytrack/tests/CreateCalendarEventTests.java
@@ -1,26 +1,39 @@
 package vytrack.tests;
 
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import utils.Driver;
+import utils.TestBase;
+import vytrack.pages.CalendarEventsPage;
 import vytrack.pages.CreateCalendarEventPage;
+import vytrack.pages.LoginPage;
 
-public class CreateCalendarEventTests extends CreateCalendarEventPage {
+public class CreateCalendarEventTests extends TestBase {
 
-    @Test(description = "Verify owners name equals to Stephan Tremaine Haley (it works on qa1 storemanager85 ")
+    @Test(description = "Verify owners name is equals to Pearl Wuckert (it works on qa1 storemanager85)")
     public void test1() {
+        LoginPage loginPage = new LoginPage();
+        CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
+        CreateCalendarEventPage createCalendarEventPage = new CreateCalendarEventPage();
 
-        // login as Stephan Tremaine Haley (storemanager85)
-        loginQA1();
+        //login as Stephan Haley (storemanager85)
+        loginPage.loginQA1();
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
 
         //go to calendar events page
-        navigateTo("Activities", "Calendar Events");
+        loginPage.navigateTo("Activities", "Calendar Events");
 
-        // click on create calendar event button
-        clickToCreateCalendarEvent();
+        //click on create calendar event button
+        calendarEventsPage.waitUntilLoaderMaskDisappear();
+        calendarEventsPage.clickToCreateCalendarEvent();
 
+        calendarEventsPage.waitUntilLoaderMaskDisappear();
         String expectedOwner = "Stephan Tremaine Haley";
-        String actualOwner = getOwnerName(expectedOwner).trim();
+        String actualOwner = createCalendarEventPage.getOwnerName(expectedOwner);
 
         Assert.assertEquals(actualOwner, expectedOwner);
+
+
     }
 }
