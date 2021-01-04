708abef63cfeeb1ed1d73bb21541d00fe8f3ce46
pkouliev <pkouliev@gmail.com>
2021-01-04 15:33:28 -0500

    updated

--- src/test/java/utils/BrowserUtils.java
+++ src/test/java/utils/BrowserUtils.java
@@ -3,42 +3,30 @@ package utils;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.*;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
 import java.util.Date;
+import java.util.function.Function;
 
-public class BrowserUtils extends Driver {
-    public static WebDriver driver = getDriver();
-    public static WebDriverWait wait = new WebDriverWait(driver, 10);
+public class BrowserUtils {
 
-    @FindBy(css = "div[class='loader-mask shown']")
-    public static WebElement loaderMask;
+    public static void space() {
 
-    public BrowserUtils() {
-        // this method requires to provide WebDriver object for @FindBy
-        PageFactory.initElements(driver, this);
+        System.out.println("\n================================================================================================\n");
     }
 
-    // This method will be used to pause our test execution
-    // provide number of seconds as a parameters
+    //It will be used to pause our test execution
+    //just provide number of seconds as a parameter
     public static void wait(int seconds) {
         try {
             Thread.sleep(1000L * seconds);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-    }
-
-
-    public static void space() {
-
-        System.out.println("\n================================================================================================\n");
     }
 
     /**
@@ -72,7 +60,7 @@ public class BrowserUtils extends Driver {
      * @return element once visible
      */
     public static WebElement waitForVisibility(WebElement element, int timeToWaitInSec) {
-        WebDriverWait wait = new WebDriverWait(getDriver(), timeToWaitInSec);
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), timeToWaitInSec);
         return wait.until(ExpectedConditions.visibilityOf(element));
     }
 
@@ -82,8 +70,8 @@ public class BrowserUtils extends Driver {
      * @param element page element needs to be clicked
      */
     public static void clickWithJS(WebElement element) {
-        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].scrollIntoView(true);", element);
-        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].click();", element);
+        ((JavascriptExecutor) Driver.getDriver()).executeScript("arguments[0].scrollIntoView(true);", element);
+        ((JavascriptExecutor) Driver.getDriver()).executeScript("arguments[0].click();", element);
     }
 
     /**
@@ -93,41 +81,32 @@ public class BrowserUtils extends Driver {
      * @param timeout time to wait
      * @return element once clickable
      */
-    public static WebElement waitForClickability(WebElement element, int timeout) {
-        wait = new WebDriverWait(getDriver(), timeout);
+    public static WebElement waitForClickablility(WebElement element, int timeout) {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), timeout);
         return wait.until(ExpectedConditions.elementToBeClickable(element));
     }
 
-    /**
+    /*
      * takes screenshot
      * whenever you call this method
-     * it takes screenshot and returns location of the screenshot
-     *
-     * @param name for target file whatever you like
-     * @return a path to screenshot takes
+     * takes screenshot and returns location of the screenshot
+     * @param name of test or whatever your like
+     * take a name of a test and returns a path to screenshot takes
      */
-    public String getScreenshot(String name) {
-        // name the screenshot with the current date to avoid duplicate name
-        // String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
-        // System.getProperty("user.dir") returns path to the project as a string
-        SimpleDateFormat df = new SimpleDateFormat("-yyyy-MM-dd-HH-mm");
+    public static String getScreenshot(String name) {
+        //name the screenshot with the current date time to avoid duplicate name
+        //String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH-mm"));
+        SimpleDateFormat df = new SimpleDateFormat("-yyyy-MM-dd HH-mm");
         String date = df.format(new Date());
-
         // TakesScreenshot ---> interface from selenium which takes screenshots
-        TakesScreenshot ts = (TakesScreenshot) driver;
+        TakesScreenshot ts = (TakesScreenshot) Driver.getDriver();
         File source = ts.getScreenshotAs(OutputType.FILE);
-
         // full path to the screenshot location
-        // where screenshot will be stored
-        // System
-        String target = System.getProperty("user.dir") + "/test-output/Screenshots" + name + date + ".png";
-
-        // if it doesn't take screenshot in anyway, remove date and time part
-        // for some users it makes problems
-        String target2 = System.getProperty("user.dir") + "/test-output/Screenshots" + name + ".png";
-
+        //System.getProperty("user.dir") returns path to the project as a string
+        String target = System.getProperty("user.dir") + "/test-output/Screenshots/" + name + date + ".png";
+        //for some user date makes problems
+        //String target = System.getProperty("user.dir") + "/test-output/Screenshots/" + name +".png";
         File finalDestination = new File(target);
-
         // save the screenshot to the path given
         try {
             FileUtils.copyFile(source, finalDestination);
@@ -138,33 +117,97 @@ public class BrowserUtils extends Driver {
     }
 
     /**
-     * While this loading screen present, html code is not complete
-     * Some elements can be missing
-     * Also, you won't be able to interact with any elements
-     * All actions will be intercepted
-     * Waits until loader mask (loading bar, spinning wheel) disappears
-     * <p>
-     * // @return true if loader mask is gone, false if it something went wrong
+     * Wait 15 seconds with polling interval of 200 milliseconds then click
+     *
+     * @param webElement of element
      */
-    public static void waitUntilLoaderMaskDisappear() {
-
-        if (driver.findElements(By.cssSelector("div[class='loader-mask shown']")).size() > 0) {
-
-            //loaderMask = driver.findElement(By.cssSelector("div[class='loader-mask shown']"));
-            wait.until(ExpectedConditions.invisibilityOf(loaderMask));
+    public static void clickWithWait(WebElement webElement) {
+        Wait wait;
+        wait = new FluentWait(Driver.getDriver())
+                .withTimeout(Duration.ofSeconds(15))
+                .pollingEvery(Duration.ofMillis(200))
+                .ignoring(NoSuchElementException.class)
+                .ignoring(ElementNotVisibleException.class)
+                .ignoring(ElementClickInterceptedException.class)
+                .ignoring(StaleElementReferenceException.class)
+                .ignoring(WebDriverException.class);
+        WebElement element = (WebElement) wait.until((Function<WebDriver, WebElement>) driver -> webElement);
+        try {
+            element.click();
+        } catch (WebDriverException e) {
+            System.out.println(e.getMessage());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            element.click();
         }
-//
-//        try {
-//            wait.until(ExpectedConditions.invisibilityOf(loaderMask));
-//            return true;
-//        } catch (NoSuchElementException e) {
-//            System.out.println("Loader mask not found");
-//            System.out.println(e.getMessage());
-//            return true; // no loader mask, all good, return true
-//        } catch (WebDriverException e) {
-//            System.out.println(e.getMessage());
-//        }
-//        return false;
     }
+
+    /**
+     * waits for backgrounds processes on the browser to complete
+     *
+     * @param timeOutInSeconds wait time
+     */
+    public static void waitForPageToLoad(long timeOutInSeconds) {
+        ExpectedCondition<Boolean> expectation = driver -> ((JavascriptExecutor) driver).executeScript("return document.readyState").equals("complete");
+        ExpectedCondition<Boolean> expectation2 = driver -> ((JavascriptExecutor) driver).executeScript("return jQuery.active == 0").equals(true);
+        try {
+            WebDriverWait wait = new WebDriverWait(Driver.getDriver(), timeOutInSeconds);
+            wait.until(expectation);
+            wait.until(expectation2);
+        } catch (Throwable error) {
+            error.printStackTrace();
+        }
+    }
+
+    public static void waitForPageTitle(String pageTitle) {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        wait.until(ExpectedConditions.titleIs(pageTitle));
+
+    }
+
+    //marufcan's
+    public static void clickWithWait(By by, int attempts) {
+        int counter = 0;
+        //click on element as many as you specified in attempts parameter
+        while (counter < attempts) {
+            try {
+                //selenium must look for element again
+                clickWithJS(Driver.getDriver().findElement(by));
+                //if click is successful - then break
+                break;
+            } catch (WebDriverException e) {
+                //if click failed
+                //print exception
+                //print attempt
+                e.printStackTrace();
+                ++counter;
+                //wait for 1 second, and try to click again
+                wait(1);
+            }
+        }
+    }
+
+    public static void waitForPresenceOfElement(By by, long time) {
+        new WebDriverWait(Driver.getDriver(), time).until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    public static void scrollToElement(WebElement element) {
+        ((JavascriptExecutor) Driver.getDriver()).executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    public static void clickWithTimeOut(WebElement element, int timeout) {
+        for (int i = 0; i < timeout; i++) {
+            try {
+                element.click();
+                return;
+            } catch (WebDriverException e) {
+                wait(1);
+            }
+        }
+    }
+
 
 }
