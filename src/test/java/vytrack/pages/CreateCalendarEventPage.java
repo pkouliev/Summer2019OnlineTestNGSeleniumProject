618a1d00e737ae532ca8574f1c4fe9f24dba23d1
pkouliev <pkouliev@gmail.com>
2021-01-04 15:49:57 -0500

    extended to BasePage and WebDriverWait object created within method

--- src/test/java/vytrack/pages/CreateCalendarEventPage.java
+++ src/test/java/vytrack/pages/CreateCalendarEventPage.java
@@ -3,13 +3,16 @@ package vytrack.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utils.Driver;
 
-public class CreateCalendarEventPage extends CalendarEventsPage {
+public class CreateCalendarEventPage extends BasePage {
 
     @FindBy(css = "[class='select2-chosen']")
     private WebElement owner;
 
     public String getOwnerName(String ownerName) {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 15);
         wait.until(ExpectedConditions.textToBePresentInElement(owner, ownerName));
         return owner.getText();
     }
