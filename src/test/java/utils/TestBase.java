5a9870a961dfb013c1b57fa7828954cd3ab8c874
pkouliev <pkouliev@gmail.com>
2021-01-04 19:31:41 -0500

    Extent Reports and other classes variables were changed from instance to static variables.

--- src/test/java/utils/TestBase.java
+++ src/test/java/utils/TestBase.java
@@ -4,10 +4,7 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.*;
 
 import java.io.IOException;
 
@@ -24,11 +21,11 @@ public abstract class TestBase {
     ExtentHtmlReporter html = new ExtentHtmlReporter("Extent.html");
     ExtentXReporter extentx = new ExtentXReporter("localhost");
      */
-    protected ExtentReports extentReports;
+    protected static ExtentReports extentReports;
     // The ExtentSparkReporter creates a rich standalone spark file.
-    protected ExtentSparkReporter extentSparkReporter;
+    protected static ExtentSparkReporter extentSparkReporter;
     // Defines a test. You can add logs, snapshots, assign author and categories to a test and its children.
-    protected ExtentTest extentTest;
+    protected static ExtentTest extentTest;
 
     @BeforeTest
     public void beforeTest() {
