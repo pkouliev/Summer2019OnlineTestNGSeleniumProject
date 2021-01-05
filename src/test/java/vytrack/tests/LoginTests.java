6251b11809e971446b4c296694fde486ec225bdc
pkouliev <pkouliev@gmail.com>
2021-01-05 12:36:29 -0500

    BrowserUtils removed from imported packages

--- src/test/java/vytrack/tests/LoginTests.java
+++ src/test/java/vytrack/tests/LoginTests.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import utils.BrowserUtils;
 import utils.ConfigurationReader;
 import utils.Driver;
 import utils.TestBase;
