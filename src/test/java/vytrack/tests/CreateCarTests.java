b0c1df24034a9570055ff0a16eb6f5e59a552b54
pkouliev <pkouliev@gmail.com>
2021-01-07 22:22:44 -0500

    Excel data file used to provide values which can be sent in same test without changing test script. List and Map data structures compared with pros and cons, and even given example how they can be combined for looping in sending data values into the site boxes.

--- src/test/java/vytrack/tests/CreateCarTests.java
+++ src/test/java/vytrack/tests/CreateCarTests.java
@@ -1,10 +1,15 @@
 package vytrack.tests;
 
 import org.testng.annotations.Test;
-import utils.TestBase;
+import utils.BrowserUtils;
+import utils.ConfigurationReader;
+import utils.ExcelUtil;
 import vytrack.pages.CreateCarPage;
 import vytrack.pages.LoginPage;
 import vytrack.pages.VehiclesPage;
+
+import java.util.List;
+import java.util.Map;
 
 public class CreateCarTests extends TestBase {
 
@@ -30,5 +35,56 @@ public class CreateCarTests extends TestBase {
 
         extentTest.pass("New car was created");
 
+    }
+
+    @Test(description = "Create a car by reading test data from excel file")
+    public void createCarTest() {
+        extentTest = extentReports.createTest("Create a car by reading test data from excel file");
+
+        LoginPage loginPage = new LoginPage();
+        VehiclesPage vehiclesPage = new VehiclesPage();
+        CreateCarPage createCarPage = new CreateCarPage();
+
+        String username = ConfigurationReader.getValue("user_name");
+        String password = ConfigurationReader.getValue("password");
+
+        loginPage.login(username, password);
+
+        loginPage.navigateTo("Fleet", "Vehicles");
+
+        loginPage.waitUntilLoaderMaskDisappear();
+
+        vehiclesPage.clickToCreateCar();
+
+        loginPage.waitUntilLoaderMaskDisappear();
+
+        ExcelUtil excelUtil = new ExcelUtil("cars.xlsx", "cars");
+
+        // read data from excel spreadsheet as List of Map
+        // testData it's just reference variable
+        List<Map<String, String>> testData = excelUtil.getDataList();
+
+        // 0 means data from first row, License Plate, it's a column name
+        // so we are reading from first row and License Plate column
+        createCarPage.licensePlateElement.sendKeys(testData.get(0).get("License Plate"));
+
+        // enter driver info
+        createCarPage.driverElement.sendKeys(testData.get(0).get("Driver"));
+        // enter model year
+        createCarPage.modelYearElement.sendKeys(testData.get(0).get("Model Year"));
+        // enter color
+        createCarPage.colorElement.sendKeys(testData.get(0).get("Color"));
+
+        loginPage.waitUntilLoaderMaskDisappear();
+        // click to save and close
+        createCarPage.saveAndCloseButtonElement.click();
+
+        BrowserUtils.wait(3); // for demo
+        extentTest.info("Created a new car");
+
+        // with many rows of data - just example
+        for (Map<String, String> value : testData) {
+            createCarPage.licensePlateElement.sendKeys(value.get("License Plate"));
+        }
     }
 }
